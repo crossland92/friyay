4406363a7d5273c798cec06f5cef4757995a0e8e
cameronsmart <camcars@hotmail.co.uk>
2016-11-04 12:40:43 +0000

    push sprint 3

---
+++ FourSidedMayhem/FourSidedMayhem/src/test/LineTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.Point;
+
+public class LineTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLength(){
+		Point point = new Point (0,10);
+		
+	}
+}
