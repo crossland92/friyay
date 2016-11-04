3d598a6dfb25dd9f33cfd5068aad86d80db994a8
en9svb <en9svb@gmail.com>
2016-11-04 12:49:32 +0000

    *Fixed PoinTest.

--- FourSidedMayhem/FourSidedMayhem/src/test/PointTest.java
+++ FourSidedMayhem/FourSidedMayhem/src/test/PointTest.java
@@ -21,28 +21,27 @@ public class PointTest {
 	public void testGetX() {
 		Point point = new Point(10.0, 10.0);
 		double retrievedValue = point.getX();
-		assertEquals(retrievedValue, 10.0, 0.00); // Does the retrievedValue equal 10.00, with the precision of 0.00
+		assertEquals(10.0, retrievedValue, 0.00); // Does the retrievedValue equal 10.00, with the precision of 0.00
 	}
 
 	@Test
 	public void testGetY() {
 		Point point = new Point(10.0, 10.0);
 		double retrievedValue = point.getY();
-		assertEquals(retrievedValue, 10.0, 0.00);  // Does the retrievedValue equal 10.00, with the precision of 0.00
+		assertEquals(10.0, retrievedValue, 0.00);  // Does the retrievedValue equal 10.00, with the precision of 0.00
 	}
 
 	@Test
 	public void testDistanceTo() {
 		Point point = new Point(9.0, 7.0);
 		Point point2 = new Point(3.0, 2.0);
-		
 		double value = point.distanceTo(point2);
-		assertEquals(value, 7.81, 0.00);   // Does the retrievedValue equal what we expected.
+		assertEquals(7.81, value, 0.01);   // Does the retrievedValue equal what we expected.
 	}
 
 	@Test
 	public void testToString() {
 		Point point = new Point(9.0, 7.0);
-		assertEquals(point.toString(), "X:9.00, Y:7.00"); // Does the toString method print out the String as expected.
+		assertEquals(new String("X:9.0, Y:7.0"), point.toString()); // Does the toString method print out the String as expected.
 	}
 }
