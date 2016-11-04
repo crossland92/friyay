d9865b4969a8aa95d7609c8a368a6be4399227c6
en9svb <en9svb@gmail.com>
2016-11-04 14:43:51 +0000

    *Done main method.

--- FourSidedMayhem/FourSidedMayhem/src/main/TestProg.java
+++ FourSidedMayhem/FourSidedMayhem/src/main/TestProg.java
@@ -1,10 +1,41 @@
 package main;
 
+import java.util.Scanner;
+
 public class TestProg {
 
 	public static void main(String[] args) {
-		System.out.println("Please select shape: T");
 
+		Scanner scanner = new Scanner(System.in);
+		try {
+			// Get the Shape type from the User.
+			boolean needShape = true;
+			while (needShape) {
+				System.out.print("Please select shape (t = Trap): ");
+				String input = scanner.next();
+				if (input.equals("T") || input.equals("t")) {
+					needShape = false;
+				}
+			}
+			
+			Point[] points = new Point[4];
+
+			int numberOfPoints = 0;
+			while (numberOfPoints < 4) {
+				System.out.print("Please enter point x: ");
+				double x = scanner.nextDouble();
+				System.out.print("Please enter point y: ");
+				double y = scanner.nextDouble();
+								
+				points[numberOfPoints] = new Point(x, y);
+				System.out.println("Point " + (numberOfPoints + 1) + " created");
+				numberOfPoints++;
+			}
+
+		} catch (Exception e) {
+			System.out.println("We messed up... sorry.");
+		} finally {
+			scanner.close();
+		}
 	}
-
 }
